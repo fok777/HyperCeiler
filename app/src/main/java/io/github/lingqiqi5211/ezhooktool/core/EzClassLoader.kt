a4bbddfaa7abe2ca70bc85785b07b34b9dbc5fa4
fok777 <297633105+fok777@users.noreply.github.com>
2026-09-15 00:42:08 +0800

    fix: EzClassLoader 重复 JvmStatic

--- app/src/main/java/io/github/lingqiqi5211/ezhooktool/core/EzClassLoader.kt
+++ app/src/main/java/io/github/lingqiqi5211/ezhooktool/core/EzClassLoader.kt
@@ -3,5 +3,5 @@ package io.github.lingqiqi5211.ezhooktool.core
 /** 默认 ClassLoader 读取。 */
 internal object EzClassLoader {
     @JvmStatic
-    @JvmStatic
-    fun current(): ClassLoader = com.sevtinge.hyperceiler.compat.HookRuntime.c
+    fun current(): ClassLoader = com.sevtinge.hyperceiler.compat.HookRuntime.classLoader()
+}
